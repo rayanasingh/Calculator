052e46f7ffebd458e81a55634cfdab0b4fd67356
Rayna Singh <rayanasingh8@gmail.com>
2020-11-15 21:37:55 +0100

    calculator

---
+++ src/com/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Calculator");
+        System.out.println("Please enter two numbers");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        System.out.println("Choose an operation (+, -)");
+        char operator = input.next().charAt(0);
+        double result = 0.0;
+        switch(operator){
+            case '+' :
+                result = number1 + number2;
+                break;
+            case '-' :
+                result = number1 - number2;
+                break;
+            default:
+                System.out.println("invalid");
+                break;
+
+        }
+        System.out.println(number1 + "" + operator + ""+ number2 + "=" + result);
+        input.close();
+
+    }
+}
